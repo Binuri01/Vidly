c294570654d2e86b5d676f38c312082db4823b7a
Binuri De Silva <binuridesilva27@gmail.com>
2025-01-15 00:51:01 +0530

    Added movie and customer controllers and views

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+                new Customer { Name = "Customer2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+            return View(viewModel);
+        }
+
+    }
+
+
+}
